4dbe52cb9222a71d9c1c8b28938a1f9501d46e82
Jorge Garzon <A01707102@tec.mx>
2025-02-25 10:11:46 -0600

    Crear Middlewares

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+//Middleware
+app.use((request, response, next) => {
+    console.log('Middleware!');
+    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+    response.send('¡Hola mundo!'); //Manda la respuesta
+});
+
+app.listen(3000);
+
+app.use('/ruta', (request, response, next) => {
+    response.send('Respuesta de la ruta "/ruta"'); 
+});
