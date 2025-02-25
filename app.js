69bcbc30a1891e339717e283af21576a08d6bfce
Jorge Garzon <A01707102@tec.mx>
2025-02-25 10:22:55 -0600

    Middleware para rutas

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ app.use((request, response, next) => {
     next(); //Le permite a la petición avanzar hacia el siguiente middleware
 });
 
+//Este middleware se resgistra sólo en la ruta /ruta1
+app.use('/ruta1', (request, response, next) => {
+    response.send("Hola desde la ruta 1");
+})
+
 app.use((request, response, next) => {
     console.log('Otro middleware!');
     response.send('¡Hola mundo!'); //Manda la respuesta
 });
 
 app.listen(3000);
-
-app.use('/ruta', (request, response, next) => {
-    response.send('Respuesta de la ruta "/ruta"'); 
-});
