607627a1623ce224c791abfa30a4f8c3eb58cab5
Jorge Garzon <A01707102@tec.mx>
2025-03-03 11:32:46 -0600

    Modelos

--- Lab13/controllers/visitantes.controller.js
+++ Lab13/controllers/visitantes.controller.js
@@ -1,4 +1,4 @@
-const visitantes = [];
+const Visitante = require('../models/visitantes.models');
 
 exports.get_registrar = (request, response, next) => {
     response.render('registrar_visitante')
@@ -6,13 +6,14 @@ exports.get_registrar = (request, response, next) => {
 
 exports.post_registrar = (request, response, next) => {
     console.log(request.body);
-    visitantes.push(request.body.nombre);
-    console.log(visitantes);
+    const visitante = new Visitante(request.body.nombre);
+    visitante.save();
+    console.log(Visitante.fetchAll());
     response.redirect('/visitantes/consultar')
 };
 
 exports.get_lista = (request, response, next) => {
     response.render('consultar_visitantes', {
-      visitantes: visitantes,
+      visitantes: Visitante.fetchAll(),
     });
 };
